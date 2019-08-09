b4975a7d6d170b47991f5710bc08ad7a7492555e
Jansen <liasece@gmail.com>
2019-08-09 14:53:03 +0800

    complete logger writer

--- playermodule/boxes/player.go
+++ playermodule/boxes/player.go
@@ -5,6 +5,8 @@ import (
 )
 
 type Player struct {
+	session map[string]string
+
 	Account `json:"account"`
 	Name    string `json:"name"`
 }
@@ -12,5 +14,18 @@ type Player struct {
 func (this *Player) GetPrimaryKey() bson.M {
 	return bson.M{
 		"account.uuid": this.Account.UUID,
+	}
+}
+
+func (this *Player) GetSession() map[string]string {
+	return this.session
+}
+
+func (this *Player) MergeSession(session map[string]string) {
+	if this.session == nil {
+		this.session = make(map[string]string)
+	}
+	for k, v := range session {
+		this.session[k] = v
 	}
 }
