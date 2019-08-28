ec6f4155e8868026e29b412f1e8138bd6e4e85a2
Jansen <liasece@gmail.com>
2019-08-28 15:08:28 +0800

    fix basemodule gate handler

--- gatemodule/module.go
+++ gatemodule/module.go
@@ -18,22 +18,22 @@ type GatewayModule struct {
 func (this *GatewayModule) AfterInitModule() {
 	this.BaseModule.AfterInitModule()
 	// 当收到客户端发过来的消息时
-	this.RegRecvMsg(this.HandleClientSocketMsg)
+	this.RegRecvMsg(this.onRecvMsg)
 }
 
-func (this *GatewayModule) HandleClientSocketMsg(
+func (this *GatewayModule) onRecvMsg(
 	conn *connect.ClientConn, msgbin *msg.MessageBinary) {
 	top := &ccmd.CS_TopLayer{}
 	json.Unmarshal(msgbin.ProtoData, top)
 	this.Debug("收到TCP消息 MsgName[%s]", top.MsgName)
 	msgname := top.MsgName
 	servertype := ccmd.GetServerTypeByMsgName(msgname)
-	serverid := conn.Session.GetBindServer(servertype)
+	serverid := conn.GetBindServer(servertype)
 	if serverid == "" {
 		// 获取一个负载均衡的服务器ID
 		serverid = this.GetBalanceServerID(servertype)
 		if serverid != "" {
-			conn.Session.SetBindServer(servertype, serverid)
+			conn.SetBindServer(servertype, serverid)
 		}
 	}
 	if serverid != "" {
