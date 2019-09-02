1bf132d64394af814482ed9a4d661dd7ba50f903
Jansen <liasece@gmail.com>
2019-09-02 15:23:48 +0800

    fix web test client

---
+++ testclient/webclient/js/network/client.js
@@ -0,0 +1,78 @@
+var Client = {
+	createNew : function(handler){
+		var res = {};
+		res._handler = handler;
+		res._inmsg = false;
+		res._totalSize = 0;
+		res._msgid = 0;
+		res._dataArray = new ArrayBuffer();
+		res._onmessage = function(evt) {
+			oldBuffer = res._dataArray;
+			res._dataArray = new ArrayBuffer(oldBuffer.byteLength + evt.data.byteLength);
+			var totalBufferView = new Uint8Array(res._dataArray);
+			var oldBufferView = new Uint8Array(oldBuffer);
+			var newBufferView = new Uint8Array(evt.data);
+			totalBufferView.set(oldBufferView);
+			totalBufferView.set(newBufferView,oldBuffer.byteLength);
+			while(true){
+				if(res._inmsg == false) {
+					if(res._dataArray.byteLength < 6){
+						break
+					}
+					res._inmsg = true;
+					var dataview = new DataView(res._dataArray.slice(0,6));
+					res._totalSize = dataview.getUint32(0);
+					res._msgid = dataview.getUint16(4);
+				} else {
+					if(res._dataArray.byteLength >= res._totalSize) {
+						var data = convertArrayToString(res._dataArray.slice(6));
+						var topobj = JSON.parse(data);
+						res._dataArray = res._dataArray.slice(res._totalSize);
+						res._inmsg = false;
+						if(res._handler.onmessage != null) {
+							res._handler.onmessage(topobj.MsgName,JSON.parse(Base64.decode(topobj.Data)));
+						}
+					} else {
+						break
+					}
+				}
+			}
+		};
+		res.send = function(topic,obj) {
+			var toplayer = {
+				"MsgName":topic,
+				"Data":Base64.encode(JSON.stringify(obj)),
+			}
+			console.log(obj)
+			var byteArray = new Uint8Array(convertStringToArray(JSON.stringify(toplayer)));
+			var buffer = new ArrayBuffer(4+2+byteArray.byteLength);
+			var dataview = new DataView(buffer);
+			dataview.setUint32(0,4+2+byteArray.byteLength);
+			dataview.setUint16(4,0);
+			var head2Array = new Uint8Array(buffer,4+2);
+			head2Array.set(byteArray);
+			res.wsconn.send(buffer);
+		};
+		res.dial = function(url) {
+			res.wsconn =  new WebSocket(url);
+			res.wsconn.binaryType = "arraybuffer";
+			res.wsconn.onopen = function(evt) {
+				if(res._handler.onopen != null) {
+					return res._handler.onopen(evt);
+				}
+			};
+			res.wsconn.onclose = function(evt) {
+				if(res._handler.onclose != null) {
+					return res._handler.onclose(evt);
+				}
+			};
+			res.wsconn.onmessage = res._onmessage;
+			res.wsconn.onerror = function(evt) {
+				if(res._handler.onerror != null) {
+					return res._handler.onerror(evt);
+				}
+			};
+		}
+		return res;
+	}
+}
---
+++ testclient/webclient/js/util/string_convert.js
@@ -0,0 +1,70 @@
+function convertStringToArray(str) {
+    var arr = new Array();
+    var len, c;
+    len = str.length;
+    for(var i = 0;
+    i < len;
+    i++ ) {
+        c = str.charCodeAt(i);
+        if(c >= 0x010000 && c <= 0x10FFFF ) {
+            arr.push(((c >> 18) & 0x07) | 0xF0);
+            arr.push(((c >> 12) & 0x3F) | 0x80);
+            arr.push(((c >> 6) & 0x3F) | 0x80);
+            arr.push((c & 0x3F) | 0x80);
+        } else if(c >= 0x000800 && c <= 0x00FFFF ) {
+            arr.push(((c >> 12) & 0x0F) | 0xE0);
+            arr.push(((c >> 6) & 0x3F) | 0x80);
+            arr.push((c & 0x3F) | 0x80);
+        } else if(c >= 0x000080 && c <= 0x0007FF ) {
+            arr.push(((c >> 6) & 0x1F) | 0xC0);
+            arr.push((c & 0x3F) | 0x80);
+        } else {
+            arr.push(c & 0xFF);
+        }
+    }
+    return arr;
+}
+
+function convertArrayToString(arr){
+    var byteArray = new Uint8Array(arr);
+    var str = "", cc = 0, numBytes = 0;
+    for(var i=0, len = byteArray.length; i<len; ++i){
+        var v = byteArray[i];
+        if(numBytes > 0){
+            //2 bit determining that this is a tailing byte + 6 bit of payload
+            if((cc&192) === 192){
+                //processing tailing-bytes
+                cc = (cc << 6) | (v & 63);
+            }else{
+                throw new Error("this is no tailing-byte");
+            }
+        }else if(v < 128){
+            //single-byte
+            numBytes = 1;
+            cc = v;
+        }else if(v < 192){
+            //these are tailing-bytes
+            throw new Error("invalid byte, this is a tailing-byte")
+        }else if(v < 224){
+            //3 bits of header + 5bits of payload
+            numBytes = 2;
+            cc = v & 31;
+        }else if(v < 240){
+            //4 bits of header + 4bit of payload
+            numBytes = 3;
+            cc = v & 15;
+        }else{
+            //UTF-8 theoretically supports up to 8 bytes containing up to 42bit of payload
+            //but JS can only handle 16bit.
+            throw new Error("invalid encoding, value out of range")
+        }
+
+        if(--numBytes === 0){
+            str += String.fromCharCode(cc);
+        }
+    }
+    if(numBytes){
+        throw new Error("the bytes don't sum up");
+    }
+    return str;
+}
